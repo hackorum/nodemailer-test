0290ec861055aa45d506aacc39e0fa167fbf7468
Neil Sabde <neilsabde@Neils-MacBook-Pro.local>
2022-09-18 11:54:16 +0530

    Second Commit

--- server.js
+++ server.js
@@ -8,10 +8,40 @@ const transporter = nodemailer.createTransport({
   port: 465,
   host: "smtp.gmail.com",
   auth: {
-    user: "neilsabde2008@gmail.com",
-    pass: "xfzclqmgxdcbikdn",
+    user: "neilsabde@gmail.com",
+    pass: "rsyxfsximzeijacc",
   },
   secure: true,
+});
+
+app.post("/send-mail", (req, res) => {
+  const to = req.body.to;
+  const name = req.body.name;
+  const amount = req.body.amount;
+  const date = req.body.date;
+  const mailData = {
+    from: "neilsabde@gmail.com",
+    to: to,
+    subject: "nodemailer-test",
+    html: ` <p>
+    Hello ${name},
+</p>
+<p>
+    This is a reminder email that your payment of amount - ${amount} is due on date - ${date}
+</p>
+<p>
+    Kindly make the payment before the due date to avoid any inconvenience.
+</p>
+<p>
+    Thanks and Regards,
+</p>`,
+  };
+  transporter.sendMail(mailData, (err, info) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.status(200).send({ message: "mail sent", message_id: info.messageId });
+  });
 });
 
 server.listen(process.env.PORT || 8080);
